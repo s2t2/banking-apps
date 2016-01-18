4840337954551816d9a731ada823f0b59483abb1
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-01-18 12:01:16 -0800

    observing transaction data download processes

---
+++ americanexpress.rb
@@ -0,0 +1,37 @@
+# login to https://www.americanexpress.com/ using your username and password
+
+
+
+# METHOD A)
+
+# click "Statements and Activity" from the navigation menu
+
+# close any popup windows
+
+# click "Year End Summary"
+
+# scroll down to a section called "Details of Spending"
+
+# click "Download",
+# ... then choose the "CSV" radio button,
+# ... then check "Include all transaction details (including merchant address, merchant doing business as)"
+# ... then click "Download"
+
+# locate the file ~/Downloads/Transactions.csv and move it to this directory (and rename it?)
+
+
+
+# METHOD B)
+
+# click "Statements and Activity" from the navigation menu
+
+# from the "Select Time Period" selector, select the start date and end date (MMDDYYY)
+# ... you may get a message like ... Your "from" or "to" date should not be prior to Dec 18, 2013
+
+# click "Download your Transactions"
+# ... then choose "Current View",
+# ... then choose the "CSV" radio button,
+# ... then check "Include all transaction details (including merchant address, merchant doing business as)"
+# ... then click "Download"
+
+# locate the file ~/Downloads/Transactions (1).csv and move it to this directory (and rename it based on the start and end dates)
---
+++ bankofamerica.rb
@@ -0,0 +1,10 @@
+# login to https://www.bankofamerica.com/ using your username and password
+
+# click on an Account link
+
+# click "Download"
+# ... then loop through each Transaction Period, starting with the first/earliest
+# ... then choose "Microsoft Excel format"
+# ... then click "Download Transactions"
+
+# locate ~/Downloads/stmt.csv and mv ~/Downloads/stmt.csv ~/Desktop/banking-apps/bofa_transactions_20150206.csv
